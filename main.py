b6cb5f5b326f3f68ed8eace400d29334acc86171
roycehoe <royceinbox@hotmail.com>
2023-02-21 14:47:58 +0800

    Create function that returns bong strings

--- main.py
+++ main.py
@@ -11,26 +11,45 @@ BOT_TOKEN = env_values["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = env_values["TELEGRAM_CHAT_ID"]
 
 
-
 class BellChime(Enum):
     QUARTER_BELLS = "bong bang beng bong"
     QUARTER_BELLS_REVERSED = "bong bang beng bong"
-    BIG_BEN = "BONG"
+    BIG_BEN = "BONG!"
 
-    def __str__(self):
-        return self.value
+    def __str__(self) -> str:
+        return str(self.value)
+
 
 FIFTEEN_MINUTE_CHIME = BellChime.QUARTER_BELLS
-THIRTY_MINUTE_CHIME = f'{BellChime.QUARTER_BELLS}....{BellChime.QUARTER_BELLS_REVERSED}'
-FOURTY_FIVE_MINUTE_CHIME = f'{BellChime.QUARTER_BELLS}....{BellChime.QUARTER_BELLS_REVERSED}....{BellChime.QUARTER_BELLS_REVERSED}'
-HOUR_CHIME = f'{BellChime.QUARTER_BELLS}....{BellChime.QUARTER_BELLS_REVERSED}....{BellChime.QUARTER_BELLS_REVERSED}....{BellChime.QUARTER_BELLS}'
+THIRTY_MINUTE_CHIME = f"{BellChime.QUARTER_BELLS}....{BellChime.QUARTER_BELLS_REVERSED}"
+FOURTY_FIVE_MINUTE_CHIME = f"{BellChime.QUARTER_BELLS}....{BellChime.QUARTER_BELLS_REVERSED}....{BellChime.QUARTER_BELLS_REVERSED}"
+HOUR_CHIME = f"{BellChime.QUARTER_BELLS}....{BellChime.QUARTER_BELLS_REVERSED}....{BellChime.QUARTER_BELLS_REVERSED}....{BellChime.QUARTER_BELLS}"
 
 
-async def test():
-    url = 'https://jsonplaceholder.typicode.com/posts/1'
-    response = requests.get(url)
-    data = response.json()
-    bot = telegram.Bot(token=BOT_TOKEN)
-    await bot.send_message(chat_id=CHAT_ID, text=str(data))
+# def get_bongs(bong_count: int) -> str:
+#     bongs = []
+#     for i in range(bong_count):
+#         bongs.append(BellChime.BIG_BEN.value)
+#     return "\n".join(bongs)
 
-asyncio.run(test())
+
+def get_bongs(bong_count: int) -> str:
+    return "\n".join([BellChime.BIG_BEN.value for i in range(bong_count)])
+    bongs = []
+    for i in range(bong_count):
+        bongs.append(BellChime.BIG_BEN.value)
+    return "\n".join(bongs)
+
+
+print(get_bongs(10))
+
+
+# async def test() -> None:
+#     url = "https://jsonplaceholder.typicode.com/posts/1"
+#     response = requests.get(url)
+#     data = response.json()
+#     bot = telegram.Bot(token=BOT_TOKEN)
+#     await bot.send_message(chat_id=CHAT_ID, text=str(data))
+
+
+# asyncio.run(test())
