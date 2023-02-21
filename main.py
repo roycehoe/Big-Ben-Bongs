1173b5a85c43a9f34512ef0e719d80f9536854f4
roycehoe <royceinbox@hotmail.com>
2023-02-21 15:19:02 +0800

    Implement logic to ouput correct chime string for current time in London

--- main.py
+++ main.py
@@ -4,52 +4,77 @@ from enum import Enum
 from dotenv import dotenv_values
 import requests
 import telegram
+from datetime import datetime
+import pytz
 
 
 env_values = dotenv_values(".env")
 BOT_TOKEN = env_values["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = env_values["TELEGRAM_CHAT_ID"]
 
+""""
+bong bahng beng bong?
+bong bahng beng bong.
+bong bahng beng bong,
+bong bahng beng bong.
+
+
+la la le lo
+lo la le lo
+la la le lo
+lo la le lo
+
+"""
+
 
 class BellChime(Enum):
-    QUARTER_BELLS = "bong bang beng bong"
-    QUARTER_BELLS_REVERSED = "bong bang beng bong"
+    FIFTEEN_MINUTE_CHIME = "bong bahng beng bong?"
+    THIRTY_MINUTE_CHIME = "bong bahng beng bong?\nbong bahng beng bong?"
+    FORTY_FIVE_MINUTE_CHIME = (
+        "bong bahng beng bong?\nbong bahng beng bong?\nbong bahng beng bong,"
+    )
+    HOUR_CHIME = "bong bahng beng bong?\nbong bahng beng bong?\nbong bahng beng bong,\nbong bahng beng bong."
     BIG_BEN = "BONG!"
 
     def __str__(self) -> str:
         return str(self.value)
 
 
-FIFTEEN_MINUTE_CHIME = BellChime.QUARTER_BELLS
-THIRTY_MINUTE_CHIME = f"{BellChime.QUARTER_BELLS}....{BellChime.QUARTER_BELLS_REVERSED}"
-FOURTY_FIVE_MINUTE_CHIME = f"{BellChime.QUARTER_BELLS}....{BellChime.QUARTER_BELLS_REVERSED}....{BellChime.QUARTER_BELLS_REVERSED}"
-HOUR_CHIME = f"{BellChime.QUARTER_BELLS}....{BellChime.QUARTER_BELLS_REVERSED}....{BellChime.QUARTER_BELLS_REVERSED}....{BellChime.QUARTER_BELLS}"
+from dataclasses import dataclass
 
 
-# def get_bongs(bong_count: int) -> str:
-#     bongs = []
-#     for i in range(bong_count):
-#         bongs.append(BellChime.BIG_BEN.value)
-#     return "\n".join(bongs)
+@dataclass
+class BigBenClock:
+    hour: str
+    minute: str
+    second: str
+
+    def get_bell_sounds(self) -> str:
+        current_minutes = int(self.minute)
+        if current_minutes > 15:
+            return BellChime.FIFTEEN_MINUTE_CHIME.value
+        if current_minutes > 30:
+            return BellChime.FIFTEEN_MINUTE_CHIME.value
+        if current_minutes > 45:
+            return BellChime.FIFTEEN_MINUTE_CHIME.value
+
+        bongs = "\n".join([BellChime.BIG_BEN.value for i in range(int(self.hour))])
+        return f"{BellChime.HOUR_CHIME.value}\n\n{bongs}"
 
 
-def get_bongs(bong_count: int) -> str:
-    return "\n".join([BellChime.BIG_BEN.value for i in range(bong_count)])
-    bongs = []
-    for i in range(bong_count):
-        bongs.append(BellChime.BIG_BEN.value)
-    return "\n".join(bongs)
+def get_big_ben_clock() -> BigBenClock:
+    london_tz = pytz.timezone("Europe/London")
+    time_now = datetime.now(london_tz)
+    hour, minute, second = time_now.strftime("%H:%M:%S").split(":")
+    return BigBenClock(hour=hour, minute=minute, second=second)
 
 
-print(get_bongs(10))
+async def test() -> None:
+    big_ben_clock = get_big_ben_clock()
+    bell_sounds = big_ben_clock.get_bell_sounds()
+
+    bot = telegram.Bot(token=BOT_TOKEN)
+    await bot.send_message(chat_id=CHAT_ID, text=str(bell_sounds))
 
 
-# async def test() -> None:
-#     url = "https://jsonplaceholder.typicode.com/posts/1"
-#     response = requests.get(url)
-#     data = response.json()
-#     bot = telegram.Bot(token=BOT_TOKEN)
-#     await bot.send_message(chat_id=CHAT_ID, text=str(data))
-
-
-# asyncio.run(test())
+asyncio.run(test())
