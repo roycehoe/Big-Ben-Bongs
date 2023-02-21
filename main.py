3db994f533438bb1b972e4b2145ee09ebd7613d3
roycehoe <royceinbox@hotmail.com>
2023-02-22 00:17:19 +0800

    Feat; Create scaffolding of application; current application allows users to get bus stop data directly

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
-import asyncio
-
-import telegram
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    filters,
+    MessageHandler,
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+)
 from app.bus_stop import get_bus_stop_data
-from constants import BOT_TOKEN, CHAT_ID, MY_HOME_BUS_STOP
+
+from constants import BOT_TOKEN
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+START_TEXT = """Commands
+/start - Start the application
+/help - View help menu
+/show - Shows all saved bus stops
+/new - Create a list of bus stops
+/about - View about page for this application
+
+"""
 
 
-async def main() -> None:
-    bus_stop_data = get_bus_stop_data(MY_HOME_BUS_STOP)
-    bot = telegram.Bot(token=BOT_TOKEN)
-    await bot.send_message(chat_id=CHAT_ID, text=str(bus_stop_data))
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=START_TEXT)
 
 
-asyncio.run(main())
+async def bus_stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_input = update.message.text
+    bus_stop_data = get_bus_stop_data(user_input)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=f"{bus_stop_data}"
+    )
+
+
+application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+start_handler = CommandHandler("start", start)
+get_bus_stop_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), bus_stop)
+
+application.add_handler(start_handler)
+application.add_handler(get_bus_stop_handler)
+
+application.run_polling()
