15f91b71d16de82e6e80f47fff221f425e5339d9
roycehoe <royceinbox@hotmail.com>
2023-02-21 23:09:46 +0800

    Savepoint; Creating string response

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import asyncio
+from typing import Optional, Union
 import requests
+import datetime
 
 from dotenv import dotenv_values
 import telegram
+from models.BusArrivalResponse import ArrivalTime, BusStopData, NextBusData
 
-from models import LTABusArrivalData
+from models.LTABusArrivalData import (
+    LTABusArrivalData,
+    NextBus,
+    NextBus2,
+    NextBus3,
+    Service,
+)
 
 
 env_values = dotenv_values(".env")
@@ -18,20 +27,51 @@ LTA_BUS_BASEURL = "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2"
 DEFAULT_HEADERS = {"AccountKey": LTA_API_KEY, "accept": "application/json"}
 
 
-def get_LTA_bus_arrival_data(bus_stop_code: str) -> str:
+def get_LTA_bus_arrival_data(bus_stop_code: str) -> LTABusArrivalData:
     try:
         response = requests.get(
             f"{LTA_BUS_BASEURL}?BusStopCode={bus_stop_code}", headers=DEFAULT_HEADERS
         )
-        return response.json()
+        return LTABusArrivalData(**response.json())
     except Exception:  # TODO: Implement better error handling
         raise Exception("Something went wrong with the LTA endpoint")
 
 
+def get_arrival_time(estimated_arrival: str) -> Optional[ArrivalTime]:
+    try:
+        time = datetime.datetime.fromisoformat(estimated_arrival)
+        return ArrivalTime(hour=time.hour, minute=time.minute, second=time.second)
+    except ValueError:  # No arrival time provided as bus is not running
+        return None
+
+
+def get_estimated_arrival(service: Service) -> list[Union[ArrivalTime, None]]:
+    return [
+        get_arrival_time(service.next_bus.estimated_arrival),
+        get_arrival_time(service.next_bus2.estimated_arrival),
+        get_arrival_time(service.next_bus3.estimated_arrival),
+    ]
+
+
+def get_next_bus_data(service: Service) -> NextBusData:
+    return NextBusData(
+        service_no=service.service_no, estimated_arrival=get_estimated_arrival(service)
+    )
+
+
+def get_bus_stop_data(bus_stop_code: str) -> BusStopData:
+    LTA_bus_arrival_data = get_LTA_bus_arrival_data(bus_stop_code)
+    next_buses = [get_next_bus_data(data) for data in LTA_bus_arrival_data.services]
+    return BusStopData(
+        bus_stop_code=LTA_bus_arrival_data.bus_stop_code, next_buses=next_buses
+    )
+
+
 async def main() -> None:
-    bus_stop_data = get_LTA_bus_arrival_data(MY_HOME_BUS_STOP)
+    bus_stop_data = get_bus_stop_data(MY_HOME_BUS_STOP)
     bot = telegram.Bot(token=BOT_TOKEN)
-    await bot.send_message(chat_id=CHAT_ID, text=bus_stop_data)
+    print(bus_stop_data)
+    # await bot.send_message(chat_id=CHAT_ID, text=bus_stop_data.__str__())
 
 
 asyncio.run(main())
